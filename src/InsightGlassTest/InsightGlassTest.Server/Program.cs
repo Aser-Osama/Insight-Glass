78738d27080ba6ab9d244c8c7780a8d4928fc7f3
Aser-Osama <117906529+Aser-Osama@users.noreply.github.com>
2024-05-06 00:53:41 +0300

    database connected

--- src/InsightGlassTest/InsightGlassTest.Server/Program.cs
+++ src/InsightGlassTest/InsightGlassTest.Server/Program.cs
@@ -1,4 +1,11 @@
 
+using InsightGlassTest.Server.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MySqlConnector;
+using System.Security.Claims;
+
 namespace InsightGlassTest.Server
 {
     public class Program
@@ -14,10 +21,56 @@ namespace InsightGlassTest.Server
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+            var connectionString = builder.Configuration.GetConnectionString("conn");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 34));
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseMySql(connectionString, serverVersion), ServiceLifetime.Scoped);
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"), options => options.EnableRetryOnFailure()), ServiceLifetime.Scoped);
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddIdentityCore<ApplicationUser>(options =>
+            {
+
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Lockout.MaxFailedAccessAttempts = 999;
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddSignInManager()
+            .AddDefaultTokenProviders();
+
             var app = builder.Build();
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.MapIdentityApi<ApplicationUser>();
+
+
+            app.MapPost("/logout", async (SignInManager<ApplicationUser> signInManager) =>
+            {
+
+                await signInManager.SignOutAsync();
+                return Results.Ok();
+
+            }).RequireAuthorization();
+
+
+            app.MapGet("/pingauth", (ClaimsPrincipal user) =>
+            {
+                var email = user.FindFirstValue(ClaimTypes.Email); // get the user's email from the claim
+                return Results.Json(new { Email = email }); ; // return the email as a plain text response
+            }).RequireAuthorization();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -29,7 +82,6 @@ namespace InsightGlassTest.Server
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
