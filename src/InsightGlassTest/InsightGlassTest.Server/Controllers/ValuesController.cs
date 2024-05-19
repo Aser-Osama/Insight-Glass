9da845d1029c54e942c96c0b793f7166254c5d4c
Aser-Osama <117906529+Aser-Osama@users.noreply.github.com>
2024-05-19 06:00:44 +0300

    added register info backend

---
+++ src/InsightGlassTest/InsightGlassTest.Server/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using InsightGlassTest.Server.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InsightGlassTest.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly idbcontext _context;
+
+        public RegisterController(idbcontext context)
+        {
+            _context = context;
+        }
+
+        // GET api/<RegisterController>/5
+        [HttpGet("{id}")]
+        [Authorize]
+        public string Get(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                return "PLEASE PROVIDE EMAIL";
+            
+            var user = _context.Aspnetusers.FirstOrDefault(x => x.Id == id);
+            if (user is null)
+                return "user not found";
+
+            //if (!user.LoggedInBefore)
+            //    return "First time login";
+            
+            if(!string.IsNullOrEmpty(user.UserType))
+                return user.UserType;
+                
+            if (_context.Companies.Any(x=>x.CompanyUserId == user.Id))
+            {
+                user.UserType = "Company";
+                _context.SaveChangesAsync();
+                return "Company";
+            }
+            else if (_context.Seekers.Any(x=> x.SeekerUserId == user.Id))
+            {
+                user.UserType = "Seeker";
+                _context.SaveChangesAsync();
+                return "Seeker";
+            }
+
+            return "First time login";
+
+        }
+    }
+}
---
+++ src/InsightGlassTest/InsightGlassTest.Server/Migrations/idbcontextMigrations/20240519022239_regPath.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsightGlassTest.Server.Migrations.idbcontextMigrations
+{
+    /// <inheritdoc />
+    public partial class regPath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "LoggedInBefore",
+                table: "aspnetusers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserType",
+                table: "aspnetusers",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_0900_ai_ci")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LoggedInBefore",
+                table: "aspnetusers");
+
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                table: "aspnetusers");
+        }
+    }
+}
